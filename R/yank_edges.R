556553f26fbf9c6be409793cb4b081a9538f67ce
Ben Domingue <ben.domingue@gmail.com>
2013-03-10 21:06:38 -0400

    adding some stuff.

--- R/yank_edges.R
+++ R/yank_edges.R
@@ -2,6 +2,7 @@ yank_edges<-function(ids,path.2.files,tmp.dir) {
   #ids needs to be a Nx2 matrix of pair ids that are of interest.
   if (length(list.dirs(tmp.dir))>0) system(paste("rm -r",tmp.dir))
   system(paste("mkdir",tmp.dir))
+  system(paste("gunzip -c ",path.2.files," > ",tmp.dir," tmp.grm",sep=""))
   yank.fun<-function(y,id) {
     grep(y[1],id[,2])->n1
     grep(y[2],id[,2])->n2
@@ -10,7 +11,7 @@ yank_edges<-function(ids,path.2.files,tmp.dir) {
     n[2]->n2
     paste("^",n1,"[[:space:]]",n2,"[[:space:]]",sep="")->str
     paste(n1,n2,sep="_")->fn
-    paste("grep '",str,"' ",path.2.files," > ",tmp.dir,"/",fn,sep="")->cmd
+    paste("grep '",str,"' tmp.grm > ",tmp.dir,"/",fn,sep="")->cmd
     system(cmd)
     TRUE
   }
@@ -23,6 +24,7 @@ yank_edges<-function(ids,path.2.files,tmp.dir) {
   apply(ids,1,yank.fun,id=gcta.id)
   getwd()->save.dir
   setwd(tmp.dir)
+  system("rm tmp.grm")
   system("cat * > sub.grm")
   read.table("sub.grm")->sub.grm
   setwd(save.dir)  
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 a package to extract portions of the genetic relatedness matrix produced by gcta.
 
+#sample of how to use the package based on gcta sample files.
+#bash
+head -n 30 test.fam > keep.txt
+gcta64 --bfile test --keep keep.txt --make-grm --out test
+
+#r
+c(11,21,51,61)->ids
+GRMextractor<-function(ids,path.2.files,tmp.dir="/tmp/GRMextractor") {
 
