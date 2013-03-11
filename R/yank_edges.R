03e7909e87ec0bb7f1a247b21c6e3605c09eb1be
Ben Domingue <ben.domingue@gmail.com>
2013-03-10 21:51:29 -0400

    everything seems to work now...

--- R/yank_edges.R
+++ R/yank_edges.R
@@ -2,16 +2,17 @@ yank_edges<-function(ids,path.2.files,tmp.dir) {
   #ids needs to be a Nx2 matrix of pair ids that are of interest.
   if (length(list.dirs(tmp.dir))>0) system(paste("rm -r",tmp.dir))
   system(paste("mkdir",tmp.dir))
-  system(paste("gunzip -c ",path.2.files," > ",tmp.dir," tmp.grm",sep=""))
+  paste("gunzip -c ",path.2.files,".gz > ",tmp.dir,"/tmp.grm",sep="")->cmd
+  system(cmd)
   yank.fun<-function(y,id) {
-    grep(y[1],id[,2])->n1
-    grep(y[2],id[,2])->n2
+    grep(paste("^",y[1],"$",sep=""),id[,2])->n1
+    grep(paste("^",y[2],"$",sep=""),id[,2])->n2
     sort(c(n1,n2),decreasing=TRUE)->n
     n[1]->n1
     n[2]->n2
     paste("^",n1,"[[:space:]]",n2,"[[:space:]]",sep="")->str
     paste(n1,n2,sep="_")->fn
-    paste("grep '",str,"' tmp.grm > ",tmp.dir,"/",fn,sep="")->cmd
+    paste("grep '",str,"' ",tmp.dir,"/tmp.grm > ",tmp.dir,"/",fn,sep="")->cmd
     system(cmd)
     TRUE
   }
@@ -29,5 +30,7 @@ yank_edges<-function(ids,path.2.files,tmp.dir) {
   read.table("sub.grm")->sub.grm
   setwd(save.dir)  
   system(paste("rm -r ",tmp.dir))
+  gcta.id[,2][sub.grm[,1]]->sub.grm[,1]
+  gcta.id[,2][sub.grm[,2]]->sub.grm[,2]
   sub.grm
 }
--- README.md
+++ README.md
@@ -5,7 +5,12 @@ a package to extract portions of the genetic relatedness matrix produced by gcta
 head -n 30 test.fam > keep.txt
 gcta64 --bfile test --keep keep.txt --make-grm --out test
 
-#r
-c(11,21,51,61)->ids
-GRMextractor<-function(ids,path.2.files,tmp.dir="/tmp/GRMextractor") {
+#R
+library(GRMextractor)
+path.2.files<-"~/wd/test.grm"
+tmp.dir<-"/tmp/GRMextractor"
+GRMextractor(c(11,21,51,61),path.2.files=path.2.files,tmp.dir=tmp.dir)
+GRMextractor(matrix(c(11,21,51,61),2,2,byrow=TRUE),path.2.files=path.2.files,tmp.dir=tmp.dir)
+GRMextractor(matrix(c(21,11,61,51),2,2,byrow=TRUE),path.2.files=path.2.files,tmp.dir=tmp.dir)
+
 
