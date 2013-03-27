267024edc030f701a1cfd954cc73829e0917c7d5
Ben Domingue <ben.domingue@gmail.com>
2013-03-27 08:59:24 -0600

    updating the way a vector of ids is handled.

--- R/GRMextractor.R
+++ R/GRMextractor.R
@@ -1,9 +1,16 @@
-GRMextractor<-function(ids,path.2.files,tmp.dir="/tmp/GRMextractor",np=1) {
+GRMextractor<-function(ids,path.2.files,tmp.dir="/tmp/GRMextractor",np=1,inbreed=FALSE) {
   #ids is either a vector or a matrix with 2 columns.
   #if a vector, then a matrix of the genetic relatedness between those individuals is formed
   #if a matrix, then an edgelist of those id pairs is formed.
   as.matrix(ids)->ids
-  if (ncol(ids)==1) {
+  if (inbreed==FALSE & ncol(ids)==1) {
+    as.character(ids)->ids
+    tmp<-list()
+    for (i in 1:length(ids)) cbind(ids[i],ids[-i])->tmp[[i]]
+    do.call("rbind",tmp)->tmp
+    tmp->ids
+  }
+  if (inbreed==TRUE & ncol(ids==1)) {
     as.character(ids)->ids
     cbind(ids,ids)->ids
   }
